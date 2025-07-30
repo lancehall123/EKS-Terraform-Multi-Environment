39978bb77dd3ca36004f4bbc639463f5e1328151
Lance Hall <lancehall123@gmail.com>
2025-07-30 10:42:14 +0100

    Adding in remote state module

---
+++ modules/remote_state/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
