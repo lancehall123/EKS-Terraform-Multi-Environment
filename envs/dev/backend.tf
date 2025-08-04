aea19d6e920a962a6aa815834d219c3518f9a6c8
Lance Hall <lancehall123@gmail.com>
2025-08-04 08:24:10 +0100

    Migrating to use terraform workspaces.

--- envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-eks-state-lance"
-    key            = "dev/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-eks-state-lance-locks"
-    encrypt        = true
-  }
-}
