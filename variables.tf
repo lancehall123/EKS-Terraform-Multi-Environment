8bfb658d5d59e42a24770f9a0144e421db3c3cd9
Lance Hall <lancehall123@gmail.com>
2025-08-05 09:56:48 +0100

    Updating variables to have default settings removing the values from the local tfvars file.

--- variables.tf
+++ variables.tf
@@ -1,12 +1,41 @@
 variable "region" {}
-variable "cluster_name" {}
-variable "node_group_name" {}
+variable "node_group_name" {
+  type = string
+  description = "Node group name"
+  default = "practice-node-group"
+}
 variable "instance_types" {
   type = list(string)
 }
-variable "desired_capacity" {}
-variable "min_size" {}
-variable "max_size" {}
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+  default     = "practice-eks"
+}
+
 variable "env" {
-  
+  type        = string
+  description = "The environment name (dev, stage, prod)"
+  default     = terraform.workspace
+}
+
+locals {
+  environment_settings = {
+    dev = {
+      desired_capacity = 1
+      max_size         = 2
+      min_size         = 1
+      instance_types   = ["t3.medium"]
+      node_group_name  = "practice-node-group"
+    }
+    prod = {
+      desired_capacity = 3
+      max_size         = 6
+      min_size         = 2
+      instance_types   = ["t3.large"]
+      node_group_name  = "prod-node-group"
+    }
+  }
+
+  settings = local.environment_settings[var.env]
 }
